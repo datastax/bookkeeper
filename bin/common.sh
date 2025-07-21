1d7a5283ddb3ac31814fe399b495ea3697a32112
priyanshu-ctds <ext-priyanshu.pareta@datastax.com>
2025-07-21 17:28:19 +0530

    fix cherry-pick : corrected required module names and removed dead link
    (cherry picked from commit dca89a720a77e7e59663b3cd737cf7ba6fe1c06a)

--- bin/common.sh
+++ bin/common.sh
@@ -133,8 +133,8 @@ CLI_GC_OPTS=${CLI_GC_OPTS:-"${DEFAULT_CLI_GC_OPTS}"}
 CLI_GC_LOGGING_OPTS=${CLI_GC_LOGGING_OPTS:-"${DEFAULT_CLI_GC_LOGGING_OPTS}"}
 
 # module names
-BOOKIE_SERVER_MODULE_NAME="(org.apache.bookkeeper-)?bookkeeper-server"
-TABLE_SERVICE_MODULE_NAME="(org.apache.bookkeeper-)?stream-storage-server"
+BOOKIE_SERVER_MODULE_NAME="(com.datastax.oss-)?bookkeeper-server"
+TABLE_SERVICE_MODULE_NAME="(com.datastax.oss-)?stream-storage-server"
 
 is_released_binary() {
   if [ -d ${BK_HOME}/lib ]; then
